aa4dcdfe3753dc79f0c287ac76d09cb0ed191d52
Renzo Gort <rgort10@Gort.UX534FT>
2020-02-06 17:24:10 +0100

    post works

--- cgi-bin/api.c
+++ cgi-bin/api.c
@@ -4,37 +4,25 @@
 #include <mariadb/mysql.h>
 #include "tables.h"
 
+void finish_with_error(MYSQL*);
 void errorResponse(int, char*);
 void getMethod(char[], char**);
-void actuator(char*, const char**, MYSQL*);
-void printActuatorJSON(struct actuator* actuators, int size);
+int getContentSize(char**);
+void actuator(char*, const char**);
+void insertDatabase(char*);
+int validateActuator(struct actuator);
+void createInsertQueryActuator(char*, struct actuator);
+struct actuator postActuator();
+int selectQueryJSON(char*, char*);
 
-void finish_with_error(MYSQL *con)
-{
-  errorResponse(500, mysql_error(con));
-  mysql_close(con);
-  exit(1);        
-}
+int CONTENT_SIZE = 0;
 
 int main(int argc, const char* argv[], char* env[]) {
-
-  MYSQL *con = mysql_init(NULL);
-
-  if (con == NULL)
-  {
-      printf("mysql_init() failed\n");
-      return 0;
-  }  
-  
-  if (mysql_real_connect(con, "localhost", "domoticati", "domoticati", "domoticati", 0, NULL, 0) == NULL) 
-  {
-      finish_with_error(con);
-  }    
-
   int i = 0;
 
   char METHOD[10];
   char REQUEST_REDIRECT[100];
+  CONTENT_SIZE = getContentSize(env);
 
   /*Get Method from request*/
   getMethod(METHOD, env);
@@ -43,7 +31,7 @@ int main(int argc, const char* argv[], char* env[]) {
     memcpy(REQUEST_REDIRECT, argv[1], 99);
 
     if(strncmp(REQUEST_REDIRECT, "actuator", 8) == 0){
-      actuator(METHOD, argv, con);
+      actuator(METHOD, argv);
     } else {
       errorResponse(404, "Didn't found requested url");
     }
@@ -53,91 +41,256 @@ int main(int argc, const char* argv[], char* env[]) {
     errorResponse(400, "Bad Request didn't recieve any parameters.");
   }
 
-  mysql_close(con);
-  
   return 0;
+}
+
+void finish_with_error(MYSQL *con) {
+  char error[150];
+  strncpy(error, mysql_error(con), 150);
+  errorResponse(500, error);
+  mysql_close(con);
+  exit(1);        
 }
 
 void errorResponse(int statusCode, char* message) {
   printf("STATUS: %d\n", statusCode);
   printf("Content-Type: application/json\n\n");
   printf("{\"errorMessage\": \"%s\"}", message);
+  exit(0);
 }
 
 void getMethod(char method[], char **env) {
   int index = 0;
   while(env[index] != NULL) {
-    if(strncmp(env[index], "REQUEST_METHOD=", 15) == 0)
+    if(strncmp(env[index], "REQUEST_METHOD=", 15) == 0) {
       memcpy(method, env[index]+15, 9);
+    }
     index++;
   }
 }
 
-void actuator(char* method, const char** argv, MYSQL* con) {
-  struct actuator* actuators = malloc(1*sizeof(struct actuator));
-  int i = 0, jsonCount = 0;
+int getContentSize(char **env) {
+  int index = 0;
+  while(env[index] != NULL) {
+    if(strncmp(env[index], "CONTENT_LENGTH=", 15) == 0){
+      if(strlen(env[index]) > 15) {
+        return atoi(env[index]+15);
+      } else {
+        return 0;
+      }
+    }
+    index++;
+  }
+  return 0;
+}
+
+void actuator(char* method, const char** argv) {
+  char* jsonObject = malloc(8000);
+  int i = 0, jsonCount = 0, index = 0;
 
   if(memcmp(argv[1]+8, "/", 1) == 0) {
     //Further redirect
   } else {
     if(strcmp(method, "GET") == 0) {
-       if (mysql_query(con, "SELECT * FROM actuator")) 
-      {
-          finish_with_error(con);
-      }
-      
-      MYSQL_RES *result = mysql_store_result(con);
-      
-      if (result == NULL) 
-      {
-          finish_with_error(con);
-      }
-
-      int num_fields = mysql_num_fields(result);
-
-      MYSQL_ROW row;
-
-      if(num_fields == ACTUATOR_FIELDS) {
-        i = 0;
-        while ((row = mysql_fetch_row(result))) {
-          actuators[i].actuatorid = atoi(row[0]);
-          actuators[i].arduinoid = atoi(row[1]);
-          actuators[i].value = atoi(row[2]);
-          memcpy(actuators[i].type, row[3], ACTUATOR_SIZE_UNIT);
-          memcpy(actuators[i].arduinovalueid, row[4], ACTUATOR_SIZE_ARDUINOVALUEID);
-          memcpy(actuators[i].actuatorname, row[5], ACTUATOR_SIZE_ACTUATORNAME);
-          i++;           
-          actuators = realloc(actuators, (i+1) * sizeof(struct actuator));
-        } 
-
+        selectQueryJSON(jsonObject, "SELECT * FROM actuator");
         printf("Content-Type: application/json\n\n");
-        printActuatorJSON(actuators, i);
+        printf("%s", jsonObject);
 
-      } else {
-        errorResponse(500, "Struct doesn't match query output");
-      }
     } else if(strcmp(method, "POST") == 0) {
-      errorResponse(200, "METHODPOST");
+      struct actuator actuator;
+      actuator = postActuator();
+
+      if(validateActuator(actuator) > 0) {
+        char* query = malloc(200);
+        createInsertQueryActuator(query, actuator);
+        insertDatabase(query);
+      } else {
+        errorResponse(400, "validation failed.");
+      }
+
+    } else {
+      errorResponse(400, "can't do that");
     }
   }
 }
 
-void printActuatorJSON(struct actuator* actuators, int size) {
-  int count;
-  printf("{\"data\": [");
+void insertDatabase(char* query) {
+  MYSQL *con = mysql_init(NULL);
 
-  for (count = 0; count < size; count++) {
-    printf("{");
-    printf("\"%s\": %d,", ACTUATOR_FIELD_NAMES[0], actuators[count].actuatorid);
-    printf("\"%s\": %d,", ACTUATOR_FIELD_NAMES[1], actuators[count].arduinoid);
-    printf("\"%s\": %d,", ACTUATOR_FIELD_NAMES[2], actuators[count].value);
-    printf("\"%s\": \"%s\",", ACTUATOR_FIELD_NAMES[3], actuators[count].type);
-    printf("\"%s\": \"%s\",", ACTUATOR_FIELD_NAMES[4], actuators[count].arduinovalueid);
-    printf("\"%s\": \"%s\"", ACTUATOR_FIELD_NAMES[5], actuators[count].actuatorname);          
-    printf("}");
-    if(count+1 != size) {
-      printf(",");
+  if (con == NULL) {
+      errorResponse(500, "mysql_init() failed");
+  }  
+  
+  if (mysql_real_connect(con, "localhost", "domoticati", "domoticati", "domoticati", 0, NULL, 0) == NULL) {
+      finish_with_error(con);
+  }    
+  
+  if (mysql_query(con, query)) {  
+      finish_with_error(con);
+  }
+
+  
+  mysql_close(con);
+  errorResponse(200, "actuator created");
+}
+
+int validateActuator(struct actuator actuator) {
+
+  if(actuator.actuatorid != -1) { //user is not allowed to create own id.
+    return -1;
+  }
+
+  if(actuator.arduinoid > 0) { //uncomment following piece when arduino table is active.
+    /*char* query = malloc(100);
+    char* response = malloc(2000);
+    sprintf(query, "SELECT id FROM arduino WHERE id = %d", actuator.actuatorid);
+    if(selectQueryJSON(response, query) <= 0) {
+      return -1;
+    }*/
+  } else {
+    return -1;
+  }
+
+  actuator.value = 0; //No matter what the user sends, the first value is always 0
+
+  if(strlen(actuator.type) == 0) {
+    return -1;
+  }
+
+  if(strlen(actuator.arduinovalueid) != 3) {
+    return -1;
+  }
+
+  if(strlen(actuator.actuatorname) == 0) {
+    return -1;
+  }
+
+  return 1;
+}
+
+void createInsertQueryActuator(char* query, struct actuator actuator) {
+  strcpy(query, "INSERT INTO actuator (arduinoid, value, type, arduinovalueid, actuatorname) VALUES(");
+
+  sprintf(query, "%s%d", query, actuator.arduinoid);
+  strcat(query, ",");
+  sprintf(query, "%s%d", query, actuator.value);
+  strcat(query, ",'");
+  strcat(query, actuator.type);
+  strcat(query, "','");
+  strcat(query, actuator.arduinovalueid);
+  strcat(query, "','");
+  strcat(query, actuator.actuatorname);
+  strcat(query, "')");
+}
+
+struct actuator postActuator() {
+  struct actuator newActuator = {-1,-1,-1,"","",""}; //define all elements so we can validate the struct
+  
+  char* data = malloc(CONTENT_SIZE+10);
+  int index;
+
+  fgets(data, CONTENT_SIZE, stdin);
+  for(index = 0; index < ACTUATOR_FIELDS; index++) {
+    char* dataPointer = strstr(data, ACTUATOR_FIELD_NAMES[index]);
+    if(dataPointer != NULL) {
+
+      dataPointer = dataPointer + 4 + strlen(ACTUATOR_FIELD_NAMES[index]); //+4 for: ": "
+      char* dataPointerEnd = strchr(dataPointer, '"');
+
+      int dataSize = dataPointerEnd - dataPointer;
+
+      char* retrievedData = malloc(dataSize+1);
+      strncpy(retrievedData, dataPointer, dataSize);
+      retrievedData[dataSize] = '\0';
+
+      switch (index) {
+      case 0:        
+        newActuator.actuatorid = atoi(retrievedData);
+        break;
+      
+      case 1:
+        newActuator.arduinoid = atoi(retrievedData);
+        break;
+      
+      case 2:
+        newActuator.value = atoi(retrievedData);
+        break;
+      
+      case 3:
+        strncpy(newActuator.type, retrievedData, ACTUATOR_FIELD_TYPE_SIZE);
+        break;
+      
+      case 4:
+        strncpy(newActuator.arduinovalueid, retrievedData, ACTUATOR_FIELD_ARDUINOVALUEID_SIZE);
+        break;
+      
+      case 5:
+        memcpy(newActuator.actuatorname, retrievedData, ACTUATOR_FIELD_ACTUATORNAME_SIZE);
+        break;
+      
+      default:
+        break;
+      }
     }
   }
-  printf("]}");
+
+  return newActuator;
+}
+
+int selectQueryJSON(char* jsonObject, char* query) {
+  MYSQL *con = mysql_init(NULL);
+
+  if (con == NULL) {
+      errorResponse(500, "mysql_init() failed");
+  }  
+  
+  if (mysql_real_connect(con, "localhost", "domoticati", "domoticati", "domoticati", 0, NULL, 0) == NULL) {
+      finish_with_error(con);
+  }    
+  
+  if (mysql_query(con, query)) {  
+      finish_with_error(con);
+  }
+  
+  MYSQL_RES *result = mysql_store_result(con);
+
+  if (result == NULL) {
+      finish_with_error(con);
+  }  
+
+  int num_fields = mysql_num_fields(result);
+  char fields[num_fields][50];
+
+  MYSQL_ROW row;
+  MYSQL_FIELD *field;
+
+  int fieldCounter = 0;
+  while(field = mysql_fetch_field(result)) {
+    strncpy(fields[fieldCounter], field->name, 50);
+    fieldCounter++;
+  }
+  
+  strcpy(jsonObject, "{\"data\": [ ");
+  while (row = mysql_fetch_row(result)) { 
+    strcat(jsonObject, "{");
+
+      for(int i = 0; i < num_fields; i++) { 
+          strcat(jsonObject, "\"");
+          strcat(jsonObject, fields[i]);
+          strcat(jsonObject, "\": \"");
+          strcat(jsonObject, row[i] ? row[i] : "NULL");
+          strcat(jsonObject, "\"");
+          strcat(jsonObject, ((i+1) != num_fields) ? "," : "");
+      }
+
+    strcat(jsonObject, "},");
+  }
+
+  jsonObject[strlen(jsonObject)-1] = ' ';
+  strcat(jsonObject, "]}\0");
+  
+  mysql_free_result(result);
+  mysql_close(con);
+
+  return strlen(jsonObject) - strlen("{\"data\": [ ]}\0");
 }
--- cgi-bin/tables.h
+++ cgi-bin/tables.h
@@ -1,16 +1,16 @@
+const char ACTUATOR_FIELD_NAMES[][30] = {"actuatorid", "arduinoid", "value", "type", "arduinovalueid", "actuatorname"};
+
+#define ACTUATOR_FIELDS 6
+#define ACTUATOR_FIELD_TYPE_SIZE 30
+#define ACTUATOR_FIELD_ARDUINOVALUEID_SIZE 5
+#define ACTUATOR_FIELD_ACTUATORNAME_SIZE 100
+
 struct actuator
 {
   int actuatorid;
   int arduinoid;
   int value;
-  char type[30];
-  char arduinovalueid[5];
-  char actuatorname[100];
+  char type[ACTUATOR_FIELD_TYPE_SIZE];
+  char arduinovalueid[ACTUATOR_FIELD_ARDUINOVALUEID_SIZE];
+  char actuatorname[ACTUATOR_FIELD_ACTUATORNAME_SIZE];
 };
-
-const char ACTUATOR_FIELD_NAMES[][30] = {"actuatorid", "arduinoid", "value", "type", "arduinovalueid", "actuatorname"};
-
-#define ACTUATOR_FIELDS 6
-#define ACTUATOR_SIZE_UNIT 30
-#define ACTUATOR_SIZE_ARDUINOVALUEID 5
-#define ACTUATOR_SIZE_ACTUATORNAME 100
